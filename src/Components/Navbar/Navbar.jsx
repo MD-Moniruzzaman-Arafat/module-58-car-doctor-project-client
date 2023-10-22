dcb5ecd68f2fa18b08d338d7d38313062240f694
md moniruzzaman arafat <arafatmonir091@gmail.com>
2023-10-22 20:29:00 +0600

    customize navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,18 @@
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { HiOutlineSearch } from "react-icons/hi";
 
 const Navbar = () => {
+
+    const menu = <>
+        <li><NavLink to={"/"}>Home</NavLink></li>
+        <li><NavLink to={"/about"}>About</NavLink></li>
+        <li><NavLink to={"/services"}>Services</NavLink></li>
+        <li><NavLink to={"/blog"}>Blog</NavLink></li>
+        <li><NavLink to={"/contact"}>Contact</NavLink></li>
+    </>
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -9,38 +21,22 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {menu}
                     </ul>
                 </div>
                 <a className="btn btn-ghost normal-case text-xl">
-                    <img src={logo} alt="" />
+                    <img src={logo} alt="" className='w-16' />
                 </a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {menu}
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <button><HiOutlineShoppingBag></HiOutlineShoppingBag></button>
+                <button className='mx-5'><HiOutlineSearch></HiOutlineSearch></button>
+                <button className="btn btn-outline btn-accent">Appointment</button>
             </div>
         </div>
     );
